a557e2975cfb443471aa675f6e709eb25186d0fe
sudarshankapali <sudarshankapalisk@gmail.com>
2023-10-06 13:51:10 +0545

    day-4

--- day4/main.js
+++ day4/main.js
@@ -1,31 +1,48 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
+let totalNumber = Math.random() * (15 - 5) + 5;
+// let xDir = 5, yDir = 2;
+// let totalNumber = 1;
 
 class Particle{
     constructor(){
-        this.xPos = 50;
-        this.yPos = 50;
-        this.radius = 20;
-        // this.render();
+        this.xPos = Math.random() * (400);
+        this.yPos = Math.random() * (400);
+        this.radius = Math.random() * (50 - 10) + 10;
+        this.xDir = Math.random() * 5;
+        this.yDir = Math.random() * 5;
     }
     render(){
         context.beginPath();
-        context.fillStyle = "red";
+        context.fillStyle = 'hsl(' + 360 * Math.random() + ', 50%, 50%)';
+        // context.fillStyle = getRandomColor();
         context.arc(this.xPos, this.yPos, this.radius, 0, 360);
         context.fill();
         this.move();
     }
     move(){
-        this.xPos += Math.random() * (1 - -1) + -1;
-        this.yPos += Math.random() * (1 - -1) + -1;
+        this.xPos += this.xDir;
+        if(this.xPos > 400 || this.xPos < 0){
+            this.xDir *= -1;
+        }
+        this.yPos += this.yDir;
+        if(this.yPos > 400 || this.yPos < 0){
+            this.yDir *= -1;
+        }
     }
 }
-const particleObject = new Particle();
+const particleArray = [];
+for(let i = 0; i <= totalNumber; i++){
+    const particleObj = new Particle();
+    particleArray.push(particleObj);
+}
 
 //game loop
 function loop(){
     context.clearRect(0, 0, 400, 400);
-    particleObject.render();
+    for(let i = 0; i <= totalNumber; i++){
+        particleArray[i].render();
+    }
     window.requestAnimationFrame(loop);
 }
 
