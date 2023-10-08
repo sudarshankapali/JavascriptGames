f385da1226fb279f002b9647d82433f32b6fcdf6
sudarshankapali <sudarshankapalisk@gmail.com>
2023-10-08 23:04:04 +0545

    drawing hand

--- mini-project/main.js
+++ mini-project/main.js
@@ -10,6 +10,7 @@ function draw() {
     context.fill();
     drawFace(context,radius);
     drawNumbers(context,radius);
+    drawTime(context,radius);
 }
 function drawFace(context, radius) {
     const grad = context.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
@@ -45,5 +46,31 @@ function drawNumbers (context, radius){
         context.translate(0,radius * 0.85);
         context.rotate(-angle);
     }
+}
+function drawTime(context, radius){
+    const now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    hour = hour%12;
+    hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
+    drawHand(context,hour,radius*0.5,radius*0.07);
+
+    minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(context,minute,radius*0.8.radius*0.07);
+
+    second = (second*Math.PI/30);
+    drawHand(context,second,radius * 0.9,radius*0.02);
+}
+function drawHand(context,position,length,width){
+    context.beginPath();
+    context.lineWidth = width;
+    context.lineCap = "round";
+    context.moveTo(0,0);
+    context.rotate(position);
+    context.lineTo(0,-length);
+    context.stroke();
+    context.rotate(-position);
 }
 draw();
