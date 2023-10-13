8fea52743e8635bff2125af1115e6fe2af1e4032
sudarshankapali <sudarshankapalisk@gmail.com>
2023-10-13 12:29:37 +0545

    spaceship image and movement done

--- day6/main.js
+++ day6/main.js
@@ -2,11 +2,14 @@ let p = document.getElementById("play");
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 let soun = new Audio();
-let sprite = new Image();
+const sprite = new Image();
 sprite.src = "./assets/rename.jpg";
 soun.src = "./assets/music.wav";
 p.addEventListener("click",()=>{
     soun.play();
 })
-context.draw(sprite,0,0,50,50);
-context.fill();
+
+    context.clearRect(0,0,canvas.width,canvas.height);
+    context.drawImage(sprite, 0, 0, 50, 50);
+
+// context.fill();
