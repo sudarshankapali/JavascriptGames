1c8879488c0ad2e58a5e652f58d17454971468ee
sudarshankapali <sudarshankapalisk@gmail.com>
2023-10-16 12:48:19 +0545

    added score

--- day9&10/main.js
+++ day9&10/main.js
@@ -1,6 +1,8 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
+let score = 0;
+
 const flappybird = new Bird();
 const bg = new Background();
 const bg1 = new Background(bg.size.width,0);
@@ -12,10 +14,24 @@ function animate(){
     bg.update(flappybird.isDead);
     bg1.update(flappybird.isDead);
     flappybird.update();
+
+    context.beginPath();
+    context.fillStyle = "white";
+    context.font = "30px sans serif";
+    context.fillText(score,10,40);
+
+    if(flappybird.isDead){
+        clearInterval(intervalId);
+    }
     window.requestAnimationFrame(animate);
 }
 
 document.addEventListener("keydown",()=>{
     flappybird.jump();
 })
+
+const intervalId = setInterval(()=>{
+    score++;
+},1000);
+
 animate();
