108d2f2756beffa73498bdd533596fb64eefde07
sudarshankapali <sudarshankapalisk@gmail.com>
2023-10-16 12:40:04 +0545

    background done

--- day9&10/main.js
+++ day9&10/main.js
@@ -2,12 +2,15 @@ let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
 const flappybird = new Bird();
-
+const bg = new Background();
+const bg1 = new Background(bg.size.width,0);
 
 
 // game loop
 function animate(){
     context.clearRect(0,0,canvas.width, canvas.height);
+    bg.update(flappybird.isDead);
+    bg1.update(flappybird.isDead);
     flappybird.update();
     window.requestAnimationFrame(animate);
 }
